45070fb74fd13bf7e682852498df362eaf838089
j0nas <jonas.jensen@msn.com>
2015-06-12 13:31:34 +0200

    Added one test

--- src/test/java/no/wact/jenjon13/assignment1/customer/CarCustomerTest.java
+++ src/test/java/no/wact/jenjon13/assignment1/customer/CarCustomerTest.java
@@ -1,9 +1,14 @@
 package no.wact.jenjon13.assignment1.customer;
 
+import no.wact.jenjon13.assignment1.car.LeaseCar;
+import no.wact.jenjon13.assignment1.leaser.CarLeaser;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Optional;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class CarCustomerTest {
     private final String customerName = "TEST";
@@ -12,6 +17,7 @@ public class CarCustomerTest {
     @Before
     public void setUp() throws Exception {
         customer = new CarCustomer(customerName);
+
     }
 
     @Test
@@ -24,5 +30,28 @@ public class CarCustomerTest {
     public void testToString() throws Exception {
         assertEquals("The toString method should return the expected value.",
                 "Customer{customerName='" + customerName + "'}", customer.toString());
+    }
+
+    @Test
+    public void testLifeCycle() throws Exception {
+        final CarCustomer testCustomer = new CarCustomer("TEST");
+
+        CarLeaser carLeaser = CarLeaser.getInstance();
+        carLeaser = null;
+        carLeaser = CarLeaser.getInstance();
+
+        final Thread thread = new Thread(testCustomer);
+        thread.start();
+        Thread.sleep(3500);
+        thread.stop();
+
+
+        final Optional<LeaseCar> carIfPresent = carLeaser.getCars()
+                .parallelStream()
+                .filter(car -> car.getLeasedTimes() > 0)
+                .findAny();
+
+        assertTrue("During the thread's lifecycle, it should lease and terminate lease of at least one car,",
+                carIfPresent.isPresent());
     }
 }
